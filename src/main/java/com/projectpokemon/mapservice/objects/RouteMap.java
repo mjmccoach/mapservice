a3b08485ce6174113f330de73189d0840cc27d88
Matthew <m.mccoach88@gmail.com>
2026-01-06 14:46:42 +0000

    MMD-87 : MINOR : Update rowmapper with component annotation. Add port to application.yml. Update routemap to have all args constructor.

--- src/main/java/com/projectpokemon/mapservice/objects/RouteMap.java
+++ src/main/java/com/projectpokemon/mapservice/objects/RouteMap.java
@@ -4,9 +4,11 @@ import com.projectpokemon.mapservice.enums.MapType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 public class RouteMap {
 
